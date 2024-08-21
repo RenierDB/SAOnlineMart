33ad1123c26fa539aec01b1ee5f40cd1c757afe5
Dinaaaaaaaaaaaaaa <rdebruin117@gmail.com>
2024-08-21 16:19:01 +0200

    Implemented sql testing scripts

---
+++ sqlscripts/testAccount.sql
@@ -0,0 +1,8 @@
+﻿DELETE FROM account;
+
+INSERT INTO account (account_name, account_email, account_password) VALUES 
+('Renier de Bruin', 'rdbemail@gmail.com', 'rdb101$#'),
+('User 2', 'user2@gmail.com', 'user2$#'),
+('User 3', 'user3@gmail.com', 'user3$#');
+
+SELECT * FROM account WHERE account_email = 'rdbemail@gmail.com' AND account_password = 'rdb101$#'
