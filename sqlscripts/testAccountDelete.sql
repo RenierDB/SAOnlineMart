1313a28a96c626880f928c9530c94b1d076c4a5d
Dinaaaaaaaaaaaaaa <rdebruin117@gmail.com>
2024-08-26 10:33:15 +0200

    Account testing script added

---
+++ sqlscripts/testAccountDelete.sql
@@ -0,0 +1,6 @@
+﻿--INSERT INTO product (product_name, product_desc, product_price) VALUES ('Putor', 'computor', 22.99);
+
+DELETE FROM product where product_name = 'Putor';
+
+SELECT * FROM product WHERE product_id = 9;
+
