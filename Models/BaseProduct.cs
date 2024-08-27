a5afea687747cce05d81fa883941d21df8790a02
Dinaaaaaaaaaaaaaa <rdebruin117@gmail.com>
2024-08-27 10:40:27 +0200

    Refactored product with SOLID

---
+++ Models/BaseProduct.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SAOnlineMart.Models
+{
+    public interface IBaseProduct
+    {
+        int Id { get; set; }
+        string? Name { get; set; }
+        decimal Price { get; set; }
+    }
+
+    public interface IProduct
+    {
+        string? Description { get; set; }
+        string? ImageUrl { get; set; }
+    }
+
+    public interface IOrderProduct
+    {
+        int Quantity { get; set; }
+    }
+
+    public class BaseProduct : IBaseProduct
+    {
+        public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Name { get; set; }
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,22 +1,15 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using SAOnlineMart.Models;
 
 namespace SAOnlineMart.Models
 {
-    public class Product
+    public class Product : BaseProduct, IProduct
     {
-        public int Id { get; set; }
-        [StringLength(60, MinimumLength = 3)]
-        [Required]
-        public string? Name { get; set; }
         [StringLength(255, MinimumLength = 3)]
         [Required]
         public string? Description { get; set; }
-        [Range(1, 100)]
-        [DataType(DataType.Currency)]
-        [Column(TypeName = "decimal(18, 2)")]
-        public decimal Price { get; set; }
         [StringLength(255, MinimumLength = 3)]
         [Required]
         public string? ImageUrl { get; set; }
