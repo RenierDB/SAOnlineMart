539a814fe869aeebe9fed154c848c4a4edd5ea49
Dinaaaaaaaaaaaaaa <rdebruin117@gmail.com>
2024-08-29 10:20:54 +0200

    Finalized admin implementations.

--- Data/SAOnlineMartContext.cs
+++ Data/SAOnlineMartContext.cs
@@ -54,7 +54,17 @@ namespace SAOnlineMart.Data
             modelBuilder.Entity<Product>().HasData(
                 new Product { Id = 1, Name = "Product1", Description = "Description1", Price = 10.0m, ImageUrl = "https://placehold.co/800x800" },
                 new Product { Id = 2, Name = "Product2", Description = "Description2", Price = 20.0m, ImageUrl = "https://placehold.co/800x800" },
-                new Product { Id = 3, Name = "Product3", Description = "Description3", Price = 30.0m, ImageUrl = "https://placehold.co/800x800" }
+                new Product { Id = 3, Name = "Product3", Description = "Description3", Price = 30.0m, ImageUrl = "https://placehold.co/800x800" },
+                new Product { Id = 4, Name = "Product4", Description = "Description4", Price = 40.0m, ImageUrl = "https://placehold.co/800x800" },
+                new Product { Id = 5, Name = "Product5", Description = "Description5", Price = 50.0m, ImageUrl = "https://placehold.co/800x800" },
+                new Product { Id = 6, Name = "Product6", Description = "Description6", Price = 60.0m, ImageUrl = "https://placehold.co/800x800" },
+                new Product { Id = 7, Name = "Product7", Description = "Description7", Price = 70.0m, ImageUrl = "https://placehold.co/800x800" },
+                new Product { Id = 8, Name = "Product8", Description = "Description8", Price = 80.0m, ImageUrl = "https://placehold.co/800x800" },
+                new Product { Id = 9, Name = "Product9", Description = "Description9", Price = 90.0m, ImageUrl = "https://placehold.co/800x800" },
+                new Product { Id = 10, Name = "Product10", Description = "Description10", Price = 100.0m, ImageUrl = "https://placehold.co/800x800" },
+                new Product { Id = 11, Name = "Product11", Description = "Description11", Price = 110.0m, ImageUrl = "https://placehold.co/800x800" },
+                new Product { Id = 12, Name = "Product12", Description = "Description12", Price = 120.0m, ImageUrl = "https://placehold.co/800x800" },
+                new Product { Id = 13, Name = "Product13", Description = "Description13", Price = 130.0m, ImageUrl = "https://placehold.co/800x800" }
             );
         }
     }
